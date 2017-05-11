23c20a89d2f14c600109578360e3b45d8a587705
Egor Pomazai <pmz-work@yandex.ru>
2017-05-11 23:09:34 +0300

    Heroku test.

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Test!');
+});
+
+app.listen(port, function () {
+    console.log(`Server running on ${port}`);
+});
