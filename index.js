8862f4bedc2f2127e7e0619adeea3d72fc002182
Egor Pomazai <pmz-work@yandex.ru>
2017-05-12 14:15:32 +0300

    sds

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Test!');
+    res.send('Test 2!');
 });
 
 app.listen(port, function () {
